58a2bb3ceca80e11aa359a78800e329a4dd66173
Olena Klementieva <foksysmile@gmail.com>
2023-03-30 20:11:08 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,36 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Olena',
+    lastname: 'Klementieva',
+  },
+
+  position: 'Junior Fullstack JS Developer',
+  salary: '60000$ per month',
+  address: 'Kiev, Ukraine',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'OlenaKlementieva@gmail.com',
+      href: 'mailto:foksysmile@gmail.com',
+    },
+    phone: {
+      text: '+380675387828',
+      href: 'tel:+380675387828',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,6 +53,202 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Summary',
+    },
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+        work on a new project I learn the domain and try to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`,
+      },
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+router.get('/skills', function (req, res) {
+  res.render('skills', {
+    page: {
+      title: 'Resume | Skills',
+    },
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'VS Code',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Git',
+          point: 10,
+          isTop: false,
+        },
+        {
+          name: 'Terminal',
+          point: 10,
+        },
+        {
+          name: 'NPM',
+          point: 10,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'Confectionery',
+          isMain: false,
+        },
+        {
+          name: 'Stunt',
+          isMain: true,
+        },
+        {
+          name: 'Writer',
+          isMain: false,
+        },
+        {
+          name: 'Ski',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+router.get('/education', function (req, res) {
+  res.render('education', {
+    page: {
+      title: 'Resume | Education',
+    },
+    header,
+
+    main: {
+      education: [
+        {
+          name: 'Primery school',
+          isEnd: true,
+        },
+        {
+          name: 'College of Sun',
+          isEnd: true,
+        },
+        {
+          name: 'University of Moon',
+          isEnd: false,
+        },
+      ],
+      certificates: [
+        {
+          name: 'Сертифікат “Шкільне життя”',
+          id: '24242542',
+        },
+        {
+          name: 'Сертифікат учасника',
+          id: '45646',
+        },
+        {
+          name: 'Сертифікат про проходження курсу',
+          id: '353646',
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+router.get('/work', function (req, res) {
+  res.render('work', {
+    layout: 'big',
+    page: {
+      title: 'Resume | Work',
+    },
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://it-brains.com.ua/',
+              about: 'Сreated a resume in training mode',
+              stack: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Node.js',
+                },
+              ],
+              stackAmount: 10,
+              awards: [
+                {
+                  name: '«WEB AWARDS UA»',
+                },
+                {
+                  name: '«WOW Awards премiя»',
+                },
+              ],
+              stackAwards: 10,
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
   })
 })
 

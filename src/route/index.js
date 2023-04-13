319df854d58e5d65393cc2f5e58a0ae7b5964f64
Olena Klementieva <foksysmile@gmail.com>
2023-04-13 19:35:21 +0300

    index8

--- src/route/index.js
+++ src/route/index.js
@@ -3646,5 +3646,17 @@ router.get('/shopcatalog', function (req, res) {
 
 // ================================================================
 
+router.get('/index', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('index', {
+    layout: 'index',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
